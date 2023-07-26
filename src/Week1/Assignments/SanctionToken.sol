abef9e61c9dca2fc6a72bae2f70cfe7a92bbd811
c-note <dongustavo20@gmail.com>
2023-07-26 16:33:41 +0100

    Add functions, events, and errors related to banned addresses.

--- src/Week1/Assignments/SanctionToken.sol
+++ src/Week1/Assignments/SanctionToken.sol
@@ -6,6 +6,18 @@ import "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
 contract SanctionToken is ERC777 {
     mapping(address => bool) private _bannedAddresses;
 
+    event AddressBanned(address indexed addr);
+    event AddressUnbanned(address indexed addr);
+
+    error SanctionToken_Address_Banned();
+    error SanctionToken_Invalid_Address();
+
+    error SanctionToken_Sender_Is_Banned();
+    error SanctionToken_Recipient_Is_Banned();
+
+    error SanctionToken_Address_Not_Banned();
+    error SanctionToken_Address_Already_Banned();
+
     constructor(
         string memory name,
         string memory symbol,
@@ -13,5 +25,29 @@ contract SanctionToken is ERC777 {
         address[] memory defaultOperators
     ) ERC777(name, symbol, defaultOperators) {
         _mint(msg.sender, initialSupply, "", "");
+    }
+
+    function banAddress(address account) external {
+        if (account == address(0)) revert SanctionToken_Invalid_Address();
+
+        if (_bannedAddresses[account])
+            revert SanctionToken_Address_Already_Banned();
+
+        _bannedAddresses[account] = true;
+        emit AddressBanned(account);
+    }
+
+    function unbanAddress(address account) external {
+        if (account == address(0)) revert SanctionToken_Invalid_Address();
+
+        if (!_bannedAddresses[account])
+            revert SanctionToken_Address_Not_Banned();
+
+        _bannedAddresses[account] = false;
+        emit AddressUnbanned(account);
+    }
+
+    function isBannedAddress(address account) external view returns (bool) {
+        return _bannedAddresses[account];
     }
 }
