81ca6e4b1ac7a29a2c52538002569bda70bdc214
c-note <dongustavo20@gmail.com>
2023-07-26 19:54:49 +0100

    Create a ERC777 token that take defaultOperators.

---
+++ src/Week1/Assignments/GodModeToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
+
+contract SanctionToken is ERC777 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address[] memory defaultOperators
+    ) ERC777(name, symbol, defaultOperators) {
+        _mint(msg.sender, initialSupply, "", "");
+    }
+}
