cac44e71dae01e3b0b47c2362a593c08a6c503a8
c-note <dongustavo20@gmail.com>
2023-07-26 16:32:05 +0100

    Add banned addresses mapping.

--- src/Week1/Assignments/SanctionToken.sol
+++ src/Week1/Assignments/SanctionToken.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.18;
 import "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
 
 contract SanctionToken is ERC777 {
+    mapping(address => bool) private _bannedAddresses;
+
     constructor(
         string memory name,
         string memory symbol,
