03e8d46093b986f62df4ce8b21d3a54bb1e5ece8
c-note <dongustavo20@gmail.com>
2023-07-29 07:30:33 +0100

    Add testFailWhenUserTryToBuyBondTokenUsingTransferAndCall().

--- test/Week1/Assignments/Bond/bondToken.t.sol
+++ test/Week1/Assignments/Bond/bondToken.t.sol
@@ -157,6 +157,20 @@ contract BondTest is Test {
         vm.stopPrank();
     }
 
+    function testFailWhenUserTryToBuyBondTokenUsingTransferAndCall() external {
+        vm.startPrank(contractDeployer);
+
+        reserveToken.freeMint();
+        reserveToken.freeMint();
+
+        bytes memory data = abi.encode(1 ether);
+
+        reserveToken.approveAndCall(address(bondToken), 2 ether, data);
+        reserveToken.transferAndCall(address(bondToken), 1 ether, data);
+
+        vm.stopPrank();
+    }
+
     function createAddress(string memory name) public returns (address) {
         address addr = address(
             uint160(uint256(keccak256(abi.encodePacked(name))))
