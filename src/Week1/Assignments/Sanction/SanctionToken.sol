3a70717c6635aafccbd565fa2efbe6a9a1186c5f
c-note <dongustavo20@gmail.com>
2023-07-31 19:15:31 +0100

    Add ISanctionToken to contract, move errors and events to interface, add comments to contract.

--- src/Week1/Assignments/Sanction/SanctionToken.sol
+++ src/Week1/Assignments/Sanction/SanctionToken.sol
@@ -1,34 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-/**
- * Created on 2023-07-30 19:26
- * @Summary A smart contract that lets admin ban addresses from sending or receiving tokens.
- * @title SanctionToken
- * @author: c-n-o-t-e
- */
-
+import {ISanctionToken} from "./ISanctionToken.sol";
 import {ERC777} from "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
 import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 import {IERC777Sender} from "openzeppelin-contracts/contracts/token/ERC777/IERC777Sender.sol";
 import {IERC777Recipient} from "openzeppelin-contracts/contracts/token/ERC777/IERC777Recipient.sol";
 
-contract SanctionToken is ERC777, AccessControl {
+/// Created on 2023-07-30 19:26
+/// @notice A smart contract that lets admin ban addresses from sending or receiving tokens.
+/// @title SanctionToken
+/// @author c-n-o-t-e
+
+contract SanctionToken is ERC777, ISanctionToken, AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
+    /// @dev mapping that holds banned address
     mapping(address => bool) private _bannedAddresses;
-
-    event AddressBanned(address indexed addr);
-    event AddressUnbanned(address indexed addr);
-
-    error SanctionToken_Address_Banned();
-    error SanctionToken_Invalid_Address();
-
-    error SanctionToken_Sender_Is_Banned();
-    error SanctionToken_Recipient_Is_Banned();
-
-    error SanctionToken_Address_Not_Banned();
-    error SanctionToken_Address_Already_Banned();
 
     constructor(
         string memory name,
@@ -40,6 +28,7 @@ contract SanctionToken is ERC777, AccessControl {
         _mint(msg.sender, initialSupply, "", "");
     }
 
+    /// @inheritdoc ISanctionToken
     function banAddress(address account) external onlyRole(ADMIN_ROLE) {
         if (account == address(0)) revert SanctionToken_Invalid_Address();
 
@@ -50,6 +39,7 @@ contract SanctionToken is ERC777, AccessControl {
         emit AddressBanned(account);
     }
 
+    /// @inheritdoc ISanctionToken
     function unbanAddress(address account) external onlyRole(ADMIN_ROLE) {
         if (account == address(0)) revert SanctionToken_Invalid_Address();
 
@@ -60,17 +50,21 @@ contract SanctionToken is ERC777, AccessControl {
         emit AddressUnbanned(account);
     }
 
-    function isBannedAddress(address account) external view returns (bool) {
+    /// @inheritdoc ISanctionToken
+    function isBannedAddress(address account) public view returns (bool) {
         return _bannedAddresses[account];
     }
 
+    /// @dev Gets called before any transfer.
+    /// @param from address sending tokens that's being checked if banned.
+    /// @param to address receiving tokens that's being checked if banned.
     function _beforeTokenTransfer(
         address,
         address from,
         address to,
         uint256
     ) internal virtual override {
-        if (_bannedAddresses[to]) revert SanctionToken_Recipient_Is_Banned();
-        if (_bannedAddresses[from]) revert SanctionToken_Sender_Is_Banned();
+        if (isBannedAddress(to)) revert SanctionToken_Recipient_Is_Banned();
+        if (isBannedAddress(from)) revert SanctionToken_Sender_Is_Banned();
     }
 }
