3a213b9f27b08ff27d6463a175d7c6fd1ed3680f
c-note <dongustavo20@gmail.com>
2023-07-31 18:32:43 +0100

    Update comments.

--- src/Week1/Assignments/GodMode/GodModeToken.sol
+++ src/Week1/Assignments/GodMode/GodModeToken.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 
-/**
- * Created on 2023-07-30 20:00
- * @Summary A smart contract that lets defaultOperators send tokens between users freely.
- * @title GodModeToken
- * @author: c-n-o-t-e
- */
-
 pragma solidity ^0.8.18;
 
 import "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
 
+/**
+ * Created on 2023-07-30 20:00
+ * @notice A smart contract that lets defaultOperators send tokens between users freely.
+ * @title GodModeToken
+ * @author: c-n-o-t-e
+ */
 contract GodModeToken is ERC777 {
     error GodMode_Default_Operators_Should_Be_Above_Zero();
 
