3106ebb2f370bfc4caf3cdd49df2a260c8911046
c-note <dongustavo20@gmail.com>
2023-07-31 18:25:59 +0100

    Add if statement in constructor

--- src/Week1/Assignments/GodMode/GodModeToken.sol
+++ src/Week1/Assignments/GodMode/GodModeToken.sol
@@ -11,6 +11,7 @@ pragma solidity ^0.8.18;
 
 import "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
 
+/// @dev Upon deploying as show in test defaultOperators should have at least one address.
 contract GodModeToken is ERC777 {
     constructor(
         string memory name,
@@ -18,6 +19,8 @@ contract GodModeToken is ERC777 {
         uint256 initialSupply,
         address[] memory defaultOperators
     ) ERC777(name, symbol, defaultOperators) {
+        if (defaultOperator.length == 0)
+            revert GodMode_Default_Operators_Should_Be_Above_Zero();
         _mint(msg.sender, initialSupply, "", "");
     }
 }
