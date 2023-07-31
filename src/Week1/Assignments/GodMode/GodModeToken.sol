e93e487e7fb6f3e815546a5fd9e73202b70dd041
c-note <dongustavo20@gmail.com>
2023-07-31 18:28:35 +0100

    Correct naming.

--- src/Week1/Assignments/GodMode/GodModeToken.sol
+++ src/Week1/Assignments/GodMode/GodModeToken.sol
@@ -12,13 +12,15 @@ pragma solidity ^0.8.18;
 import "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
 
 contract GodModeToken is ERC777 {
+    error GodMode_Default_Operators_Should_Be_Above_Zero();
+
     constructor(
         string memory name,
         string memory symbol,
         uint256 initialSupply,
         address[] memory defaultOperators
     ) ERC777(name, symbol, defaultOperators) {
-        if (defaultOperator.length == 0)
+        if (defaultOperators.length == 0)
             revert GodMode_Default_Operators_Should_Be_Above_Zero();
         _mint(msg.sender, initialSupply, "", "");
     }
