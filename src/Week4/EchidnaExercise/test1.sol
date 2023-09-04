46c97af1ef19c2249ab0752b5b602c61f19561f9
c-note <dongustavo20@gmail.com>
2023-09-04 02:37:25 +0100

    Rename file.

---
+++ src/Week4/EchidnaExercise/test1.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+import "./task.sol";
+
+contract TestToken is Task {
+    constructor() public {
+        pause(); // pause the contract
+        owner = address(0); // lose ownership
+    }
+
+    function echidna_cannot_be_unpause() public view returns (bool) {
+        return (paused() == true);
+    }
+}
