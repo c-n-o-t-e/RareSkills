a710aa41902fd0bf55145eca70ff8ff0ab7c751e
c-note <dongustavo20@gmail.com>
2023-09-08 12:07:01 +0100

    Add BondingCurve contracts for Echidna testing.

---
+++ Echidna/contracts/BondingCurve/ReserveToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "erc-payable-token/contracts/token/ERC1363/ERC1363.sol";
+
+contract ReserveToken is ERC1363 {
+    address public owner;
+
+    constructor() ERC20("ReserveToken", "RT") {
+        owner = msg.sender;
+        _mint(msg.sender, 1 ether);
+    }
+
+    function freeMint() public {
+        _mint(msg.sender, 1 ether);
+    }
+}
