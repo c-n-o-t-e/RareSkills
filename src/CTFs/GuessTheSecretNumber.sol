2d4c1a75b0f50e0dd8e92cd75bc4e39ecbffd43a
c-note <dongustavo20@gmail.com>
2023-09-18 12:11:07 +0100

    Add CTFs task and solution.

---
+++ src/CTFs/GuessTheSecretNumber.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+contract GuessTheSecretNumber {
+    bytes32 answerHash =
+        0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365;
+
+    constructor() payable {
+        require(msg.value == 1 ether);
+    }
+
+    function isComplete() public view returns (bool) {
+        return address(this).balance == 0;
+    }
+
+    function guess(uint8 n) public payable returns (bool) {
+        require(msg.value == 1 ether);
+
+        if (keccak256(abi.encodePacked(n)) == answerHash) {
+            (bool ok, ) = msg.sender.call{value: 2 ether}("");
+            require(ok, "Failed to Send 2 ether");
+        }
+        return true;
+    }
+}
+
+// Write your exploit codes below
+contract ExploitContract {
+    bytes32 answerHash =
+        0xdb81b4d58595fbbbb592d3661a34cdca14d7ab379441400cbfa1b78bc447c365;
+
+    function Exploiter() public view returns (uint8) {
+        for (uint8 n = 0; n <= 255; n++) {
+            if (keccak256(abi.encodePacked(n)) == answerHash) {
+                return n;
+            }
+        }
+        return 0;
+    }
+}
